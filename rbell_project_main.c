240907c7f430fc3867e056b3d64083cf15faadbe
BenBurger852 <benburger852@gmail.com>
2024-10-04 16:02:23 +0200

    New Compile ASUS

--- rbell_project_main.c
+++ rbell_project_main.c
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+//new compile on asus
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
